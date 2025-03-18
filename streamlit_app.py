f069860fef1ed0851fe7b80ec405be032f0b9f6e
erwinvlopez <147115784+erwinvlopez@users.noreply.github.com>
2025-03-17 21:03:51 -0500

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ from snowflake.snowpark.functions import col
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-
 name_on_order = st.text_input("Name on Smoothie: ")
 st.write("The name on your smoothie will be:", name_on_order)
 
